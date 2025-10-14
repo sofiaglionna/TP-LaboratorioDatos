5914ef5ce3dd7643bc1b426de23f34bdabeea280
Sofia <sofiaglionna@gmail.com>
2025-10-14 20:19:59 -0300

    Punto i - errores
    Muchas cosas por arreglar:
    -arreglar tabla poblacion
    -borrar total de tabla poblacion
    -En EE el tipo de dato de “departamento_id” es float64, mientras que en el resto es de tipo int64. Hay que cambiarlos porque si no cuando hacemos un inner join no se reconocen como iguales los “departamento_id”

--- TP1/tp labo/Analisis_Datos.py
+++ TP1/tp labo/Analisis_Datos.py
@@ -12,31 +12,96 @@ dfEP_con_desc = pd.read_csv("datasets/Finales/EP_con_desc.csv")
 #Ejercicio 1)
 
 # i)
-#Tomamos de dfdepartamento la provincia y departamento 
-#con el (id_departamento) asociado a departamento, contamos la cant de establecimientos educativos de dfEE por nivel (jardin, primario, secundario...)
-#y relacionamos cada nivel con la población asociada a su rango de edad de dfPoblación.
-#  El orden del reporte debe ser alfabético por provincia y dentro de las provincias descendente por cantidad de escuelas primarias.
 
-# i) Provincia, Departamento, EE por nivel (Jardines/Primarias/Secundarias) y
-#    población por grupo etario (3–5, 6–11, 12–17). Orden: provincia ASC, primarias DESC.
+# Contamos los Establecimientos Educativos en cada departamento.
+
+CantEE = """
+    SELECT
+        departamento_id,
+        SUM("Nivel inicial - Jardín maternal" + "Nivel inicial - Jardín de infantes") AS Jardines,
+        SUM("Primario") AS Primarios,
+        SUM("Secundario - INET" + "Secundario") AS Secundarios,
+        SUM("SNU" + "SNU - INET") AS SNU
+        FROM dfEE
+        GROUP BY departamento_id
+"""
+dfCantEE = dd.query(CantEE).df()
+
+# Calculamos las poblaciones de cada rango de edad según nivel educativo por departamento:
+# Población Jardín = 0 - 5
+# Población Primaria = 6 - 12
+# Población Secundaria = 13 - 18
+# SNU (Educación Adultos) = 19+ 
+
+PoblacionXJardinEnDpto = """
+    SELECT 
+        departamento_Id,
+        SUM(Casos) AS "Poblacion Jardin"
+        FROM dfPoblacion
+        WHERE Edad < 6
+        GROUP BY departamento_id
+        """
+
+dfPoblacionXJardinEnDpto = dd.query(PoblacionXJardinEnDpto).df()
+
+PoblacionXPrimarioEnDpto = """
+    SELECT 
+        departamento_Id,
+        SUM(Casos) AS "Poblacion Primaria"
+        FROM dfPoblacion
+        WHERE Edad > 5 AND Edad < 13
+        GROUP BY departamento_id
+        """
+
+dfPoblacionXPrimarioEnDpto = dd.query(PoblacionXPrimarioEnDpto).df()
+
+PoblacionXSecundarioEnDpto = """
+    SELECT 
+        departamento_Id,
+        SUM(Casos) AS "Poblacion Secundaria"
+        FROM dfPoblacion
+        WHERE Edad > 12 AND Edad < 19
+        GROUP BY departamento_id
+        """
+
+dfPoblacionXSecundarioEnDpto = dd.query(PoblacionXSecundarioEnDpto).df()
+
+PoblacionXSNUEnDpto = """
+    SELECT 
+        departamento_Id,
+        SUM(Casos) AS "Poblacion Adultos"
+        FROM dfPoblacion
+        WHERE Edad > 18
+        GROUP BY departamento_id
+        """
+
+dfPoblacionXSNUEnDpto = dd.query(PoblacionXSNUEnDpto).df()
+
+# Ahora juntamos la información de Dpto, Cant de EE por Dpto y Población x Dpto (Relacionando todas estas tablas por departamento_id)
+# Vamos a usar alias porque si no son muy largos los nombres: 
 
 i = """
+    SELECT 
+        d.provincia, 
+        d.departamento, 
+        ee.Jardines, 
+        pj."Poblacion Jardin", 
+        ee.Primarios, 
+        pp."Poblacion Primaria", 
+        ee.Secundarios, 
+        ps."Poblacion Secundaria", 
+        ee.SNU, 
+        pa."Poblacion Adultos"
+    FROM dfDepartamento AS d
+    INNER JOIN dfCantEE AS ee ON d.departamento_id = ee.departamento_id
+    INNER JOIN dfPoblacionXJardinEnDpto AS pj ON d.departamento_id = pj.departamento_id
+    INNER JOIN dfPoblacionXPrimarioEnDpto AS pp ON d.departamento_id = pp.departamento_id
+    INNER JOIN dfPoblacionXSecundarioEnDpto AS ps ON d.departamento_id = ps.departamento_id
+    INNER JOIN dfPoblacionXSNUEnDpto AS pa ON d.departamento_id = pa.departamento_id
+    """
+dfi = dd.query(i).df()
 
-"""
-
-
-jardines = """
-SELECT 
- departamento_id,
- COUNT(*) AS cantidad_jardines
- FROM dfEE
- WHERE "Nivel inicial - Jardín maternal" > 0
-   OR "Nivel inicial - Jardín de infantes" > 0
- GROUP BY departamento_id
-"""
-
-dfJardines = dd.query(jardines).df()
-print(dfJardines)
+#%%
 
 
 # ii)
